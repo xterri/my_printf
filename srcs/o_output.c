f3ddfb2cc0c7eec1203f97c68c8b27cf85038e5e
terri <thuynh1989@gmail.com>
2017-08-09 23:52:15 -0700

    adding octal output function

---
+++ srcs/o_output.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   o_output.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/09 18:50:05 by thuynh            #+#    #+#             */
+/*   Updated: 2017/08/09 20:34:30 by thuynh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		o_output(t_print **list)
+{
+	char	*oct;
+	size_t	len;
+
+// if (!(check_valid(list, 'o')))
+//		return (-1);
+// if len modifier is opened and is equal to h -> pass to short function
+// else if len modifier is opened and equal to hh -> pass to signed char func
+	// return 1 upon success and -1 on fail
+	oct = pf_itoa_base(VAL_UINT, "01234567", 8);
+	len = ft_strlen(oct);
+	if (f_exists(list, '-'))
+		LEN += write(1, oct, len);
+	MIN = MIN > len ? MIN - len : 0;
+	MIN ? put_wflags(list, MIN) : 0;
+	if (!f_exists(list, '-'))
+		LEN += write(1, oct, len);
+	free(oct);
+	oct = NULL; //necessary?
+	return (1);
+}
