5b33546f4d7b797e57710bbab2725cc0c81adab0
terri <thuynh1989@gmail.com>
2017-08-09 23:51:49 -0700

    updated di_output to handle short len modifier/flag

--- srcs/di_output.c
+++ srcs/di_output.c
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 21:35:52 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/09 14:36:49 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/09 23:49:24 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int		di_output(t_print **list)
 
 //	if (!(check_valid(list, 'd')))
 //		return (-1);
+	if (LF)
+	{
+		short_output(list);
+		return (1);
+	}
 	val = VAL_INT;
 	digits = ft_digits(val);
 	val >= 0 && (f_exists(list, '+') || f_exists(list, ' ')) ? digits++ : 0;
@@ -36,13 +41,13 @@ int		di_output(t_print **list)
 		val = put_plus(list, val);
 	if (MIN)
 	{
-		MIN = !P && MIN < digits ? 0 : MIN;
+		MIN = !P && MIN <= digits ? 0 : MIN;
 		!P && MIN > digits ? MIN -= digits : 0;
 		MIN ? put_wflags(list, MIN) : 0;
 	}
 	if (!f_exists(list, '-'))
 	{
-		WF == 1 ? val = put_plus(list, val) : 0;
+		val = put_plus(list, val);
 		P ? prec_fill(list) : 0;
 		ft_putnbr(val);
 	}
