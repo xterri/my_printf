6939dfb0d7e2dfe5f87516a12aeab937ba403213
terri <thuynh1989@gmail.com>
2017-08-09 23:53:13 -0700

    function to handle different itoa conversion for unsigned values (i.e. o, u, x, etc.)

---
+++ srcs/pf_itoa_base.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pf_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/09 18:57:17 by thuynh            #+#    #+#             */
+/*   Updated: 2017/08/09 19:01:12 by thuynh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_pf_digits(uintmax_t val, int base)
+{
+	size_t ret;
+
+	ret = 1;
+	while (val >= (uintmax_t)base)
+	{
+		val /= base;
+		ret++;
+	}
+	return (ret);
+}
+
+char	*pf_itoa_base(uintmax_t val, char *key, int base)
+{
+	char	*str;
+	size_t	digits;
+
+	digits = ft_pf_digits(val, base);
+	if (!(str = ft_strnew(digits)))
+		return (NULL);
+	str[digits] = '\0';
+	while (digits-- > 0)
+	{
+		str[digits] = key[(val % base)];
+		val /= base;
+	}
+	return (str);
+}
