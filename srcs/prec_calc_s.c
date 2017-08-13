073e301fa74a6f3d6a766c6491d875410122c4f3
terri <thuynh1989@gmail.com>
2017-08-13 11:43:25 -0700

    created separate prec_calc for strings, necessary because it handles the calculations a bit differently compared to ints and uints

---
+++ srcs/prec_calc_s.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prec_calc_s.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/13 11:15:15 by thuynh            #+#    #+#             */
+/*   Updated: 2017/08/13 11:21:15 by thuynh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	prec_calc_s(t_print **list, ssize_t len)
+{
+	if (PNB >= len)
+	{
+		if (PNB >= MIN)
+		{
+			PNB -= (PNB - MIN);
+			MIN = PNB - len;
+		}
+		else if (PNB && MIN > PNB)
+			MIN = (MIN - PNB) + (PNB - len);
+	}
+	else if (MIN > len)
+		if (!P)
+		{
+			MIN -= len;
+			!PNB ? PNB += len : 0;
+		}
+	if (PNB < len)
+		MIN = MIN > PNB ? MIN - PNB : 0;
+}
