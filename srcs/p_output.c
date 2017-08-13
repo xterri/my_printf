54edf412322570a6a511f370f1c351c93eb2f365
terri <thuynh1989@gmail.com>
2017-08-13 12:56:23 -0700

    edited p_output to print the 0 if val given is 0

--- srcs/p_output.c
+++ srcs/p_output.c
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/09 16:42:50 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/13 12:33:16 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/13 12:55:49 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ int		p_output(t_print **list)
 	if (f_exists(list, '-'))
 	{
 		LEN += write(1, "0x", 2);
-		if (!ft_strcmp(addr, "0"))
-			return (LEN = -1);
 		LEN += write(1, addr, len);
 	}
 	MIN = MIN > len ? MIN - (len + 2) : 0;
@@ -31,8 +29,6 @@ int		p_output(t_print **list)
 	if (!f_exists(list, '-'))
 	{
 		LEN += write(1, "0x", 2);
-		if (!ft_strcmp(addr, "0"))
-			return (LEN = -1);
 		LEN += write(1, addr, len);
 	}
 	free(addr);
