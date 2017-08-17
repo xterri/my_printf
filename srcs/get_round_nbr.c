33754f36c5286339ba99624229e90890e6539701
terri <thuynh1989@gmail.com>
2017-08-16 17:17:23 -0700

    small change in get_round_nbr

--- srcs/get_round_nbr.c
+++ srcs/get_round_nbr.c
@@ -6,7 +6,7 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/16 09:30:52 by thuynh            #+#    #+#             */
-/*   Updated: 2017/08/16 09:33:38 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/08/16 17:16:25 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ double	get_round_nbr(ssize_t prec)
 
 	i = 0;
 	round = 1;
-	while (i < prec)
+	while (i <= prec)
 	{
 		round /= 10;
 		i++;
